c9d15ddfe80584ef557b537b122993e5277b46ee
Anna Kijania <kijania.anna@gmail.com>
2020-09-24 11:05:41 +0200

    Add tests for OrderOption

---
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import OrderOption from './OrderOption';
+
+describe('Component OrderOption', () => {
+    
+  it('should render without crashing', () => {
+    const component = shallow(<OrderOption name='Lorem ipsum' type='text' />);
+    expect(component).toBeTruthy();
+  });
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption />);
+    expect(component).toEqual({});
+  });
+  it('should render correct title', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const component = shallow(<OrderOption name={expectedTitle} type='text' />);
+    expect(component.find('.title').text()).toEqual(expectedTitle);
+    
+  }); 
+});
