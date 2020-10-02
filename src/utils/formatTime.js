327c5c8f002a062a793ce24eaaea335edfb33612
Anna Kijania <kijania.anna@gmail.com>
2020-10-02 09:59:38 +0200

    Improve formatTime readability

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,20 +1,20 @@
 export const formatTime = time => {
   if (typeof(time) != 'number' || time < 0){
     return null;
-  } else{
-    let output = ((time%60)< 10 ? '0' : '') + time%60;
-    if(time >= 60) {
-      output = ((Math.floor(time/60)%60)< 10 ? '0' : '') +  Math.floor(time/60)%60 + ':' + output;
-    }
-    if(time >= 60*60) {
-      output = ((Math.floor(time/(60*60))%60)< 10 ? '0' : '') +  Math.floor(time/(60*60)) + ':' + output;
-    }
-    if(time<60*60) {
-      output = '00:' + output;
-    }
-    if(time<60) {
-      output = '00:' + output;
-    }
-    return output;
-  }  
-};
+  } 
+  const ONE_SECOND = 1;
+  const ONE_MINUTE_IN_SECONDS = 60;
+  const ONE_HOUR_IN_SECONDS = 60*60;
+  const getPart = (time, partTime) => ((Math.floor(time/partTime)%60)< 10 ? '0' : '') +  Math.floor(time/partTime)%60;
+
+  let seconds = getPart(time, ONE_SECOND);
+  let minutes = '00';
+  let hours = '00';
+  if (time >= ONE_MINUTE_IN_SECONDS) {
+    minutes = getPart(time, ONE_MINUTE_IN_SECONDS);
+  }
+  if (time >= ONE_HOUR_IN_SECONDS) {
+    hours = getPart(time, ONE_HOUR_IN_SECONDS);
+  }
+  return `${hours}:${minutes}:${seconds}`;
+};  
